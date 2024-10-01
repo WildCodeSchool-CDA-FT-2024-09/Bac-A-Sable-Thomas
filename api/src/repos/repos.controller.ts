0849604327aba1c26eadd103094ba08d165f43ab
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-01 15:44:40 +0200

    repo: update repocontroller to use TypeORM

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,57 +1,87 @@
 import { Request, Response } from "express";
-import repos from "../../data/repos.json";
-import { Repo } from "./repos.types";
+import { Repo } from "./repos.entity";
+import { validate } from "class-validator";
+import { QueryFailedError } from "typeorm";
 
-export const getRepos = (req: Request, res: Response) => {
+export const getRepos = async (req: Request, res: Response) => {
   // Query params
   if (req.query !== undefined) {
     if (req.query.order) {
-      const orderBy = req.query.order;
-      // testing order by "name" or "id"
-      repos.sort((a, b) => {
-        if (orderBy === "name") {
-          return a.name.localeCompare(b.name);
-        } else if (orderBy === "id") {
-          return a.id.localeCompare(b.id);
-        }
-        return 0;
-      });
-      res.status(200).json(repos);
+      const orderBy = req.query.order as string;
+      try {
+        const repos = await Repo.find({
+          order: { [orderBy]: "ASC" },
+        });
+        res.status(200).json(repos);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
     }
   }
-  // Default
-  res.status(200).json(repos);
+
+  try {
+    const repos = await Repo.find();
+    res.status(200).json(repos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-export const getRepo = (req: Request, res: Response) => {
-  const repo: Repo = repos.find((repo) => repo.id === req.params.id) as Repo;
-  if (!repo) {
+export const getRepo = async (req: Request, res: Response) => {
+  const repo = await Repo.findOne({ where: { id: req.params.id } });
+  if (repo) {
+    res.status(200).json(repo);
+  } else {
     res.status(404).json({ message: "Repo not found" });
   }
-  res.status(200).json(repo);
 };
 
-export const addRepo = (req: Request, res: Response) => {
+export const addRepo = async (req: Request, res: Response) => {
   const newRepo: Repo = req.body;
-  repos.push(newRepo);
-  res.status(201).json(newRepo);
+  try {
+    const repo = Repo.create(newRepo);
+    // Validate entity before saving
+    const errors = await validate(repo);
+    if (errors.length > 0) {
+      res.status(400).json({ message: "Validation errors", errors });
+      return;
+    }
+    // Attempt to save entity - have to use explicit insert as .save() in TypeORM does an upsert.
+    // This is somewhat particular to the demo case with a non-autoincrementing primary key.
+    await Repo.insert(repo);
+    res.status(201).json(repo);
+  } catch (err) {
+    if (err instanceof QueryFailedError) {
+      // Db constraint errors will show here
+      res
+        .status(400)
+        .json({ message: "Database query failed", details: err.message });
+    } else {
+      // Generic error handler
+      res
+        .status(500)
+        .json({ message: "Internal server error", details: err.message });
+    }
+  }
 };
 
-export const deleteRepo = (req: Request, res: Response) => {
-  const repoIndex = repos.findIndex((repo) => repo.id === req.params.id);
-  if (repoIndex === -1) {
+export const deleteRepo = async (req: Request, res: Response) => {
+  const result = await Repo.delete(req.params.id);
+  if (result.affected === 0) {
     res.status(404).json({ message: "Repo not found" });
+  } else {
+    res.status(204).send();
   }
-  repos.splice(repoIndex, 1);
-  res.status(204).send();
 };
 
-export const updateRepo = (req: Request, res: Response) => {
-  const repoIndex = repos.findIndex((repo) => repo.id === req.params.id);
-  if (repoIndex === -1) {
+export const updateRepo = async (req: Request, res: Response) => {
+  const repo = await Repo.findOne({ where: { id: req.params.id } });
+
+  if (repo) {
+    Repo.merge(repo, req.body);
+    const updatedRepo = await repo.save();
+    res.status(200).json(updatedRepo);
+  } else {
     res.status(404).json({ message: "Repo not found" });
   }
-  const updatedRepo: Repo = req.body;
-  repos[repoIndex] = updatedRepo;
-  res.status(200).json(updatedRepo);
 };
