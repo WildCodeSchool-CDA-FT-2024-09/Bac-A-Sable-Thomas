92cde1de8235d7ac74b6fffd5830089e22a4b4c9
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-01 16:04:47 +0200

    statuses: setup status entity, routes, controller for create and read, add status test requests

--- api/src/db/data-source.ts
+++ api/src/db/data-source.ts
@@ -1,13 +1,14 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 import * as dotenv from "dotenv";
 import { Repo } from "../repos/repos.entity";
+import { Status } from "../statuses/statuses.entity";
 
 dotenv.config();
 
 const dataSourceOptions: DataSourceOptions = {
   type: "sqlite",
   database: "./src/db/db.sqlite",
-  entities: [Repo],
+  entities: [Repo, Status],
   synchronize: true,
   // logging: true,
 };
--- api/src/router.ts
+++ api/src/router.ts
@@ -1,9 +1,11 @@
 // Main router definition
 import express from "express";
 import repoRouter from "./repos/repos.routes";
+import statusRouter from "./statuses/statuses.routes";
 
 const router = express.Router();
 
 router.use("/repos", repoRouter);
+router.use("/statuses", statusRouter);
 
 export default router;
