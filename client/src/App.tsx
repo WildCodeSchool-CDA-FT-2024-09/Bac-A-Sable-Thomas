b59b552ed942aec3cfc2d115ea07e4a4073b7c2b
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-02 20:30:32 +0200

    repos-list: base App layout, css clean, basic reposlist with useEffect fetch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import ReposList from "./components/ReposList";
 
 function App() {
   return (
     <>
-      <div>
-        <p className="text-3xl text-rose-600">My Lovely Sandbox</p>
+      <div className="container mx-auto flex min-h-[95vh] flex-col items-center justify-between bg-white bg-gradient-to-tl from-orange-300 to-indigo-400 p-4 px-6 shadow-lg shadow-slate-700">
+        <header className="mb-6">
+          <h1 className="mb-4 text-center text-3xl font-bold">
+            My Lovely Repos
+          </h1>
+          <p className="text-center text-gray-600">// TODO - Nav</p>
+        </header>
+        <main>
+          <ReposList />
+        </main>
+        <footer>
+          <p className="mt-4 text-center text-gray-600">
+            &copy; {new Date().getFullYear()} My Lovely Repos
+          </p>
+        </footer>
       </div>
     </>
   );
