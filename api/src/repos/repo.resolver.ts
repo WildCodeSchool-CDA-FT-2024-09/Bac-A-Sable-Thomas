6eaf0270a49cb41c7d4ad037f7cb0e63bbf2a98f
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-10 10:34:31 +0200

    api/graphql: update repo resolver to handle lang filter param on repos, repo by id

--- api/src/repos/repo.resolver.ts
+++ api/src/repos/repo.resolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Query, Resolver } from "type-graphql";
 import { Repo, RepoRestricted } from "./repos.entity";
+import { In } from "typeorm";
 
 // Input Types
 // @InputType()
@@ -20,19 +21,49 @@ import { Repo, RepoRestricted } from "./repos.entity";
 export default class RepoResolver {
   // Method GET for all repos
   @Query(() => [Repo])
-  async repos() {
+  async repos(
+    @Arg("language", { nullable: true }) language: string
+  ): Promise<Repo[]> {
+    if (language) {
+      const repos = await this.reposByLanguage(language);
+      return repos;
+    } else {
+      const repos = await Repo.find({
+        relations: { languages: true, status: true, comments: true },
+      });
+      return repos;
+    }
+  }
+
+  async reposByLanguage(language: string): Promise<Repo[]> {
     const repos = await Repo.find({
-      relations: { languages: true, status: true, comments: true },
+      relations: { languages: true },
+      where: { languages: { label: language } },
     });
-    return repos;
+
+    const fullRepos = await Repo.find({
+      where: { id: In(repos.map((repo) => repo.id)) },
+      relations: { status: true, languages: true, comments: true },
+    });
+
+    return fullRepos;
   }
 
   @Query(() => [RepoRestricted])
-  async reposRestricted() {
+  async reposRestricted(): Promise<RepoRestricted[]> {
     const repos = await Repo.find({
       relations: { status: true },
     });
     return repos;
+  }
+
+  @Query(() => Repo)
+  async repo(@Arg("id") id: string): Promise<Repo | null> {
+    const repo = await Repo.findOne({
+      relations: { languages: true, status: true, comments: true },
+      where: { id },
+    });
+    return repo;
   }
 
   // @Mutation(() => Repo)
