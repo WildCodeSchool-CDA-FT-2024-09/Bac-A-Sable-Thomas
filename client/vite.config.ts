c0fb71841ec45aaa18ebfbe7da792669056b43c6
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-29 09:13:42 +0100

    docker: intial setup with dockerfiles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", //  Listen on all network interfaces
+    port: 5173, // Port exposed in Docker
+    watch: {
+      usePolling: true,
+    },
+  },
 });
