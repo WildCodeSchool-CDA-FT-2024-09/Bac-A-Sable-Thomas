5b49b4744e77d9878b29ffb854506e2e64139bf1
Thomas Allen <thomasedwardallen@gmail.com>
2024-11-17 13:12:30 +0100

    api/docker: resolve issue with front / back build via docker compose leading to extremely slow npm i stage

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,37 +1,8 @@
-// // Entrypoint - configures the express server and starts it
+import "reflect-metadata";
 import * as dotenv from "dotenv";
-// import express from "express";
-// import cors from "cors";
-// import router from "./router";
-// import "reflect-metadata";
-// import dataSource from "./db/data-source";
 
 dotenv.config();
 const { PORT } = process.env;
-
-// const app = express();
-
-// app.use(
-//   cors({
-//     origin: FRONTEND_URL,
-//   })
-// );
-
-// // Express doesn't parse JSON by default
-// app.use(express.json());
-// app.use("/api", router);
-
-// app.listen(PORT, async () => {
-//   dataSource
-//     .initialize()
-//     .then(() => {
-//       console.log("Data Source has been initialized");
-//     })
-//     .catch((err) => {
-//       console.error("Error during Data Source initialization", err);
-//     });
-//   console.log(`Server is listening on http://localhost:${PORT}`);
-// });
 
 import { ApolloServer } from "@apollo/server"; // preserve-line
 import { startStandaloneServer } from "@apollo/server/standalone"; // preserve-line
