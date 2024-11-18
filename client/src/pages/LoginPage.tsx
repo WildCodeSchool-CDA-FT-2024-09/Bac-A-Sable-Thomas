886ac1e1c3cf01413a6c26c6805e29ae3bc52ec6
Thomas Allen <thomas-allen_student2021@wilder.school>
2024-11-18 22:37:22 +0100

    client/login: add login page, switch register to use context

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,39 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import { useRegisterMutation } from "../generated/graphql-types";
+import { useLoginLazyQuery } from "../generated/graphql-types";
 import { useAuth } from "../contexts/useAuth";
 
-function SignupPage() {
-  const [register, { loading, error }] = useRegisterMutation();
+function LoginPage() {
+  const [login, { loading, error }] = useLoginLazyQuery();
   const { setUser } = useAuth();
   const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const email = (e.currentTarget[0] as HTMLInputElement).value;
-    const username = (e.currentTarget[1] as HTMLInputElement).value;
-    const password = (e.currentTarget[2] as HTMLInputElement).value;
+    const password = (e.currentTarget[1] as HTMLInputElement).value;
 
-    const response = await register({
-      variables: { email, username, password },
+    const response = await login({
+      variables: { email, password },
       context: {
         credentials: "include", // Ensure cookies are included in the request
       },
     });
 
-    if (response.data?.register) {
-      setUser(response.data.register);
+    if (response.data?.login) {
+      setUser(response.data.login);
       navigate("/");
     }
   };
   return (
     <main className="flex h-2/3 flex-col items-center justify-start">
-      <h1 className="mb-4">Join Lovely Repos</h1>
+      <h1 className="mb-4">Login to Lovely Repos</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input className="rounded px-2 py-1" type="text" placeholder="Email" />
-        <input
-          className="rounded px-2 py-1"
-          type="text"
-          placeholder="Username"
-        />
         <input
           className="rounded px-2 py-1"
           type="password"
@@ -43,7 +37,7 @@ function SignupPage() {
           className="rounded border border-slate-700 bg-slate-300 transition-all hover:scale-110"
           type="submit"
         >
-          {loading ? "Signing up..." : "Signup"}
+          {loading ? "Logging in..." : "Login"}
         </button>
       </form>
       {error && <p className="text-red-500">{error.message}</p>}
@@ -51,4 +45,4 @@ function SignupPage() {
   );
 }
 
-export default SignupPage;
+export default LoginPage;
